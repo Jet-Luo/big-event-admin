60f24f84ae20739c56edb9f93f6dc28fd486598e
Jet-Luo <23071138@qq.com>
2025-10-16 09:08:29 +0800

    feat(store): implement user store with token management and persistence

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
-<script setup></script>
+<script setup>
+import { useUserStore } from '@/stores/user.js'
+
+const userStore = useUserStore()
+</script>
 
 <template>
   <div>app</div>
   <!-- 测试element-plus -->
   <el-button>test</el-button>
+  <!--  测试userStore-->
+  <el-button>{{ userStore.token }}</el-button>
+  <el-button @click="userStore.setToken('test token')">login</el-button>
+  <el-button @click="userStore.removeToken()">logout</el-button>
 </template>
 
 <style scoped></style>
