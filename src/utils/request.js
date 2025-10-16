4cebcd372a61956e63ca9c275e6225ec2122af21
Jet-Luo <23071138@qq.com>
2025-10-16 10:21:49 +0800

    feat(request): add axios instance with request and response interceptors

---
+++ src/utils/request.js
@@ -0,0 +1,53 @@
+import axios from 'axios'
+import { useUserStore } from '@/stores'
+import { ElMessage } from 'element-plus'
+import router from '@/router/index.js'
+
+const baseURL = 'http://big-event-vue-api-t.itheima.net'
+
+const instance = axios.create({
+  // 1. 基础地址，超时时间
+  baseURL,
+  timeout: 10000 // 10秒
+})
+
+// 请求拦截器
+instance.interceptors.request.use(
+  (config) => {
+    // 2. 携带token
+    const userStore = useUserStore()
+    if (userStore.token) {
+      config.headers.Authorization = userStore.token
+    }
+    return config
+  },
+  (err) => Promise.reject(err)
+)
+
+// 响应拦截器
+instance.interceptors.response.use(
+  (res) => {
+    // 3. 处理业务失败
+    // 4. 摘取核心响应数据
+    if (res.data.code === 0) {
+      return res
+    }
+    // 处理业务失败，提示错误，抛出错误
+    ElMessage.error(res.data.message || '请求失败')
+    return Promise.reject(res.data)
+  },
+  (err) => {
+    // 5. 处理401错误
+    // 401错误：权限不足或token过期，拦截到登录页
+    if (err.response?.status === 401) {
+      router.push('/login')
+    }
+
+    // 其他错误：提示错误，抛出错误
+    ElMessage.error(err.response.data.message || '请求失败')
+    return Promise.reject(err)
+  }
+)
+
+export default instance
+export { baseURL }
